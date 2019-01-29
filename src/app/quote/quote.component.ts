d99b990b6c2a8c1cbfa031485f1f83c4d68feab1
nuriath <mnuriath28@gmail.com>
2019-01-29 09:58:23 +0200

    modify a quotes deatils file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,14 +9,13 @@ import {Quotes} from "../quotes"
 })
 export class QuoteComponent implements OnInit {
   quotes= [
-    new Quotes(1,'When you reach the end of your rope, tie a knot in it and hang on.','Ernest Hemingway', 'Nuriath',0,0,),
-    new Quotes(2,'There is nothing permanent except change','Heraclitus','Nuriath',0,0),
-    new Quotes(3, 'You cannot shake hands with a clenched fist.','Indira Gandhi','Nuriath',0,0),
-    new Quotes(4, 'Let us sacrifice our today so that our children can have a better tomorrow.','A. P. J. Abdul Kalam','Nuriath',0,0 ),
-    new Quotes(5, 'It is better to be feared than loved, if you cannot be both.','Niccolo Machiavelli','Nuriath',0,0),
-    new Quotes(6, 'Learning never exhausts the mind.','Leonardo da Vinci','Nuriath',0,0 ),
-    new Quotes(7,'All that we see or seem is but a dream within a dream.','Edgar Allan Poe','Nuriath',0,0),
-
+    new Quotes(1,'When you reach the end of your rope, tie a knot in it and hang on.','Ernest Hemingway', 'Nuriath',0,0,new Date('2019, 03, 2')),
+    new Quotes(2,'There is nothing permanent except change','Heraclitus','Nuriath',0,0, new Date('2020, 02,16')),
+    new Quotes(3, 'You cannot shake hands with a clenched fist.','Indira Gandhi','Nuriath',0,0, new Date('2019,  06, 8')),
+    new Quotes(4, 'Let us sacrifice our today so that our children can have a better tomorrow.','A. P. J. Abdul Kalam','Nuriath',0,0, new Date('2019, 10, 20')),
+    new Quotes(5, 'It is better to be feared than loved, if you cannot be both.','Niccolo Machiavelli','Nuriath',0,0, new Date('2019, 11, 11')),
+    new Quotes(6, 'Learning never exhausts the mind.','Leonardo da Vinci','Nuriath',0,0, new Date('2020, 08, 18') ),
+    new Quotes(7,'All that we see or seem is but a dream within a dream.','Edgar Allan Poe','Nuriath',0,0, new Date('2022, 03,12'))
 ]
 
 toogleDetails(index){
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,7 @@ export class Quotes {
     public showDescription:boolean
     constructor(public id:number, 
         public name:string, 
-        public auther:string, 
+        public author:string, 
         public username:string,
         public upvote:number, 
         public downvote:number,
